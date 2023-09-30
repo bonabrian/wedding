7677fdd052e16117b42879d91e6e1610185b8262
bonabrian <bonabrian.dev@gmail.com>
2023-10-01 00:02:45 +0700

    add SEO title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
+
 import Onboarding from '@/components/onboarding'
+
+export const metadata: Metadata = {
+  title: 'Undangan Pernikahan Bona & Silvia',
+}
 
 const HomePage = () => {
   return <Onboarding />
