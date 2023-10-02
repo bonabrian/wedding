7b1661b751d157d72308a56a6e5075ae956871fb
bonabrian <bonabrian.dev@gmail.com>
2023-10-02 20:08:00 +0700

    add calendar event

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
+
 import cn from '@/lib/cn'
-import { ceremonyDate } from '@/lib/config'
+import { calendarUrl, ceremonyDate } from '@/lib/config'
 
 import BackgroundPattern from './background-pattern'
 import CountdownTimer from './countdown-timer'
 import Heading from './heading'
+import { Calendar } from './icons'
 
 const Countdown = () => {
   return (
@@ -13,8 +16,26 @@ const Countdown = () => {
         caption="Hitung Mundur"
         inverseColor
       />
-      <div className={cn('flex flex-col items-center justify-center gap-4')}>
+      <div
+        className={cn('flex flex-col items-center justify-center gap-6 z-10')}
+      >
         <CountdownTimer date={ceremonyDate} />
+
+        <Link
+          href={calendarUrl}
+          target="_blank"
+          rel="noreferrer"
+          className={cn('mt-4')}
+        >
+          <button
+            className={cn(
+              'inline-flex items-center gap-2 border-2 px-2 py-1 text-sm border-white rounded-md text-white',
+            )}
+          >
+            <Calendar className={cn('w-4 h-4')} />
+            Tambahkan ke Kalender
+          </button>
+        </Link>
       </div>
     </BackgroundPattern>
   )
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -52,6 +52,9 @@ export const introImage = '/assets/images/front-cover.png'
 // audio
 export const mainAudio = '/assets/audio/audio.mp3'
 
+export const calendarUrl =
+  'http://www.google.com/calendar/event?action=TEMPLATE&dates=20240420T010000Z%2F20240420T100000Z&text=Pernikahan%20Bona%20%26%20Silvia&location=Gedung%20R.%20A.%20Wiriadinata%2C%209%2C%20Jl.%20Terusan%20Kopo%20No.Km%2C%20RW.5%2C%20Sulaiman%2C%20Kec.%20Margahayu%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat%2040222%2C%20Indonesia&details='
+
 export const snowfall: SnowfallProps = {
   color: '#BB4A4A',
   speed: [0, 1],
