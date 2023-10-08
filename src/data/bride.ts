b731b94798582454a46dccfb73fe736dc5d77a6e
bonabrian <bonabrian.dev@gmail.com>
2023-10-09 02:57:30 +0700

    fix invalid bride data

--- src/data/bride.ts
+++ src/data/bride.ts
@@ -17,10 +17,10 @@ export const groom: Bride = {
 }
 
 export const bride: Bride = {
-  nickName: 'Bona',
-  fullName: 'Bona Brian Siagian',
-  birthOrder: 'Putra',
-  fatherName: 'Bpk. Salomo Siagian',
-  motherName: 'Ibu Monika Purba',
-  image: 'https://placehold.co/400x600/black/white',
+  nickName: 'Silvia',
+  fullName: 'Silvia Novianty Sitorus',
+  birthOrder: 'Putri',
+  fatherName: 'Bpk. Sarbini Sitorus',
+  motherName: 'Ibu Elsa Agustina Sirait ✞',
+  image: 'https://placehold.co/400x600/9f80ff/white',
 }
