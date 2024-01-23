d692f59d9108daada0b01ea8120cde9b9d234015
bonabrian <bonabrian.dev@gmail.com>
2024-01-23 19:43:34 +0700

    fix(next): deprecated config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  compress: true,
+  reactStrictMode: true,
+  crossOrigin: 'anonymous',
   images: {
-    dangerouslyAllowSVG: true,
-    domains: ['placehold.co', 'avatar.oxro.io'],
+    remotePatterns: [
+      { hostname: 'placehold.co' },
+      { hostname: 'avatar.oxro.io' },
+    ],
   },
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    })
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
