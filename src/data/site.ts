d2006bd95234b13f80f8f5d28729b6dfb434c59a
bonabrian <bonabrian.dev@gmail.com>
2024-03-14 20:42:48 +0700

    fix: calendar start time

--- src/data/site.ts
+++ src/data/site.ts
@@ -14,7 +14,7 @@ export const musicBackground = '/audio/audio.mp3'
 export const youtubeVideoID = 'y76dB_HmVjE'
 
 export const calendarUrl =
-  'http://www.google.com/calendar/event?action=TEMPLATE&dates=20240420T010000Z%2F20240420T100000Z&text=Pernikahan%20Bona%20%26%20Silvia&location=Gedung%20R.%20A.%20Wiriadinata%2C%209%2C%20Jl.%20Terusan%20Kopo%20No.Km%2C%20RW.5%2C%20Sulaiman%2C%20Kec.%20Margahayu%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat%2040222%2C%20Indonesia&details='
+  'http://www.google.com/calendar/event?action=TEMPLATE&dates=20240420T040000Z%2F20240420T110000Z&text=Pernikahan%20Bona%20%26%20Silvia&location=Gedung%20R.%20A.%20Wiriadinata%2C%209%2C%20Jl.%20Terusan%20Kopo%20No.Km%2C%20RW.5%2C%20Sulaiman%2C%20Kec.%20Margahayu%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat%2040222%2C%20Indonesia&details='
 
 export const snowfall: SnowfallProps = {
   color: '#BB4A4A',
