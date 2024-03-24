68840d969898ed6b8afe7c032a1c44bc4a83328b
Bona Brian Siagian <bonabrian.dev@gmail.com>
2024-03-24 07:32:29 +0700

    feat: adjust og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -20,143 +20,157 @@ const alexBrushRegular = fetch(
   new URL('../../../../assets/fonts/AlexBrush-Regular.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer())
 
-export const GET = async (req: NextRequest) => {
+export const GET = async (_req: NextRequest) => {
   const fontPlusJakartaSansLight = await plusJakartaSansLight
   const fontPlusJakartaSansBold = await plusJakartaSansBold
   const fontAlexBrushRegular = await alexBrushRegular
 
-  const { searchParams } = new URL(req.url)
-  const guestName = searchParams.get('guest')
+  // const { searchParams } = new URL(req.url)
+  // const guestName = searchParams.get('guest')
 
   try {
     return new ImageResponse(
       (
-        <div
-          tw="flex flex-col py-2 px-12 relative h-full"
-          style={{ backgroundColor: '#fff' }}
-        >
-          <div tw="absolute top-0 right-0 flex">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width="120"
-              height="160"
-              src="https://res.cloudinary.com/dg48wbt00/image/upload/v1698232347/flower-frame_q1qdke.png"
-              alt="frame"
-              style={{ transform: 'rotate(180deg)' }}
-            />
-          </div>
-          <div tw="absolute bottom-0 left-0 flex">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width="120"
-              height="160"
-              src="https://res.cloudinary.com/dg48wbt00/image/upload/v1698232347/flower-frame_q1qdke.png"
-              alt="frame"
-            />
-          </div>
+        <div tw="flex w-100">
           <div
-            tw="flex flex-col items-center justify-center"
-            style={{ fontFamily: 'PlusJakartaSans-Light', fontSize: '12px' }}
-          >
-            <h2
-              tw="my-0 uppercase"
-              style={{
-                fontFamily: 'PlusJakartaSans-Light',
-                paddingBottom: '4px',
-                borderBottom: '1px solid #000',
-              }}
-            >
-              Undangan Pernikahan
-            </h2>
-            {guestName && (
-              <div tw="flex flex-col justify-center items-center">
-                <p
-                  tw="mb-0 mt-2"
-                  style={{
-                    fontFamily: 'PlusJakartaSans-Light',
-                    fontSize: '14px',
-                    textAlign: 'center',
-                  }}
-                >
-                  Kepada:
-                </p>
-                <p
-                  tw="m-0"
-                  style={{
-                    fontFamily: 'PlusJakartaSans-Bold',
-                    fontSize: '16px',
-                    textAlign: 'center',
-                  }}
-                >
-                  {guestName.replace('@', '&')}
-                </p>
-              </div>
-            )}
-          </div>
-          <p
-            tw="my-2"
             style={{
-              fontFamily: 'PlusJakartaSans-Light',
-              fontSize: '14px',
-              textAlign: 'center',
+              position: 'absolute',
+              backgroundImage:
+                'url("https://res.cloudinary.com/dg48wbt00/image/upload/v1711240282/video-thumb_w5748g.jpg")',
+              display: 'block',
+              width: '100%',
+              height: '100%',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '256px 256px',
             }}
-          >
-            Kami mengundang Bapak/Ibu/Saudara/i untuk hadir di acara pernikahan
-            kami.
-          </p>
-          <div
-            tw="flex flex-col items-center justify-center mt-4"
-            style={{
-              fontFamily: 'AlexBrush-Regular',
-              fontSize: '3rem',
-            }}
-          >
-            <div
-              style={{
-                lineHeight: 0.75,
-                // letterSpacing: '0.05em',
-                marginTop: '0px',
-                marginBottom: '0px',
-              }}
-            >
-              Bona
-            </div>
-            <div
-              style={{
-                lineHeight: 0.5,
-                // letterSpacing: '0.05em',
-                marginTop: '0px',
-                marginBottom: '0px',
-              }}
-            >
-              &
-            </div>
-            <div
-              style={{
-                lineHeight: 0.75,
-                // letterSpacing: '0.05em',
-                marginTop: '0px',
-                marginBottom: '0px',
-              }}
-            >
-              Silvia
-            </div>
-          </div>
-
-          <div tw="flex flex-col justify-center items-center mt-5">
-            <div
-              style={{ fontFamily: 'PlusJakartaSans-Bold', fontSize: '16px' }}
-            >
-              24.04.2024
-            </div>
-            <div
-              style={{ fontSize: '12px', fontFamily: 'PlusJakartaSans-Light' }}
-            >
-              11:00 - 18:00 WIB
-            </div>
-          </div>
+          />
         </div>
       ),
+      // <div
+      //   tw="flex flex-col py-2 px-12 relative h-full"
+      //   style={{ backgroundColor: '#fff' }}
+      // >
+      //   <div tw="absolute top-0 right-0 flex">
+      //     {/* eslint-disable-next-line @next/next/no-img-element */}
+      //     <img
+      //       width="120"
+      //       height="160"
+      //       src="https://res.cloudinary.com/dg48wbt00/image/upload/v1698232347/flower-frame_q1qdke.png"
+      //       alt="frame"
+      //       style={{ transform: 'rotate(180deg)' }}
+      //     />
+      //   </div>
+      //   <div tw="absolute bottom-0 left-0 flex">
+      //     {/* eslint-disable-next-line @next/next/no-img-element */}
+      //     <img
+      //       width="120"
+      //       height="160"
+      //       src="https://res.cloudinary.com/dg48wbt00/image/upload/v1698232347/flower-frame_q1qdke.png"
+      //       alt="frame"
+      //     />
+      //   </div>
+      //   <div
+      //     tw="flex flex-col items-center justify-center"
+      //     style={{ fontFamily: 'PlusJakartaSans-Light', fontSize: '12px' }}
+      //   >
+      //     <h2
+      //       tw="my-0 uppercase"
+      //       style={{
+      //         fontFamily: 'PlusJakartaSans-Light',
+      //         paddingBottom: '4px',
+      //         borderBottom: '1px solid #000',
+      //       }}
+      //     >
+      //       Undangan Pernikahan
+      //     </h2>
+      //     {guestName && (
+      //       <div tw="flex flex-col justify-center items-center">
+      //         <p
+      //           tw="mb-0 mt-2"
+      //           style={{
+      //             fontFamily: 'PlusJakartaSans-Light',
+      //             fontSize: '14px',
+      //             textAlign: 'center',
+      //           }}
+      //         >
+      //           Kepada:
+      //         </p>
+      //         <p
+      //           tw="m-0"
+      //           style={{
+      //             fontFamily: 'PlusJakartaSans-Bold',
+      //             fontSize: '16px',
+      //             textAlign: 'center',
+      //           }}
+      //         >
+      //           {guestName.replace('@', '&')}
+      //         </p>
+      //       </div>
+      //     )}
+      //   </div>
+      //   <p
+      //     tw="my-2"
+      //     style={{
+      //       fontFamily: 'PlusJakartaSans-Light',
+      //       fontSize: '14px',
+      //       textAlign: 'center',
+      //     }}
+      //   >
+      //     Kami mengundang Bapak/Ibu/Saudara/i untuk hadir di acara pernikahan
+      //     kami.
+      //   </p>
+      //   <div
+      //     tw="flex flex-col items-center justify-center mt-4"
+      //     style={{
+      //       fontFamily: 'AlexBrush-Regular',
+      //       fontSize: '3rem',
+      //     }}
+      //   >
+      //     <div
+      //       style={{
+      //         lineHeight: 0.75,
+      //         // letterSpacing: '0.05em',
+      //         marginTop: '0px',
+      //         marginBottom: '0px',
+      //       }}
+      //     >
+      //       Bona
+      //     </div>
+      //     <div
+      //       style={{
+      //         lineHeight: 0.5,
+      //         // letterSpacing: '0.05em',
+      //         marginTop: '0px',
+      //         marginBottom: '0px',
+      //       }}
+      //     >
+      //       &
+      //     </div>
+      //     <div
+      //       style={{
+      //         lineHeight: 0.75,
+      //         // letterSpacing: '0.05em',
+      //         marginTop: '0px',
+      //         marginBottom: '0px',
+      //       }}
+      //     >
+      //       Silvia
+      //     </div>
+      //   </div>
+
+      //   <div tw="flex flex-col justify-center items-center mt-5">
+      //     <div
+      //       style={{ fontFamily: 'PlusJakartaSans-Bold', fontSize: '16px' }}
+      //     >
+      //       24.04.2024
+      //     </div>
+      //     <div
+      //       style={{ fontSize: '12px', fontFamily: 'PlusJakartaSans-Light' }}
+      //     >
+      //       11:00 - 18:00 WIB
+      //     </div>
+      //   </div>
+      // </div>
       // <div tw="flex w-full">
       //   <div tw="flex flex-1">
       //     <div
@@ -314,8 +328,8 @@ export const GET = async (req: NextRequest) => {
       //   </div>
       // </div>
       {
-        width: 480,
-        height: 320,
+        width: 256,
+        height: 256,
         fonts: [
           {
             name: 'PlusJakartaSans-Light',
