fe88039d0fbd96a04aa59b75710a59adbd5b9274
Bona Brian Siagian <bonabrian.dev@gmail.com>
2024-03-28 19:27:13 +0700

    feat: add preferred region

--- src/app/api/guests/route.ts
+++ src/app/api/guests/route.ts
@@ -4,6 +4,7 @@ import { findAllGuests } from '@/actions/guests'
 import { getErrorMessage, response } from '@/lib/api'
 
 export const dynamic = 'force-dynamic'
+export const preferredRegion = ['sin1']
 
 export const GET = async (_req: NextRequest) => {
   try {
